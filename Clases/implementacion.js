1d956be1fa8e0384d830f9cf0e649f0ed860cf6a
aminespinoza10 <amin.espinoza@outlook.com>
2025-02-25 12:32:23 -0600

    Ejercicio de clases agregado

---
+++ Clases/implementacion.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var clase_1 = require("./clase");
+var persona = new clase_1.Persona("Amin", 39, true);
+console.log(persona.saludar());
---
+++ Clases/implementacion.ts
@@ -0,0 +1,5 @@
+import { Persona } from "./clase";
+
+let persona = new Persona("Amin", 39, true);
+
+console.log(persona.saludar());
