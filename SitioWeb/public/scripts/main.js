044edcec6403bf695e02479b439763d7b9fbf135
aminespinoza10 <amin.espinoza@outlook.com>
2025-02-26 12:39:09 -0600

    Ejercicio de sitio web actualizado

---
+++ SitioWeb/public/scripts/main.js
@@ -0,0 +1,8 @@
+"use strict";
+const h1 = document.querySelector('h1');
+console.log(h1 === null || h1 === void 0 ? void 0 : h1.textContent);
+let title = document.querySelector('.title');
+console.log(title === null || title === void 0 ? void 0 : title.textContent);
+const message = document.querySelector('#message');
+console.log(message.placeholder);
+console.log("Hola de nuevo");
---
+++ SitioWeb/src/main.ts
@@ -0,0 +1,11 @@
+
+const h1 = document.querySelector('h1');
+console.log(h1?.textContent);
+
+let title : HTMLHeadingElement = document.querySelector('.title') as HTMLHeadingElement;
+console.log(title?.textContent);
+
+const message = document.querySelector('#message') as HTMLInputElement;
+console.log(message.placeholder);
+
+console.log("Hola de nuevo");
