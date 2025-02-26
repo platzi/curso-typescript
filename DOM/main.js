3d7d445a1fd13d51a615a6ea289626ebeb539784
aminespinoza10 <amin.espinoza@outlook.com>
2025-02-26 12:13:48 -0600

    Ejercicio DOM agregado

---
+++ DOM/main.js
@@ -0,0 +1,7 @@
+var h1 = document.querySelector('h1');
+console.log(h1 === null || h1 === void 0 ? void 0 : h1.textContent);
+var title = document.querySelector('.title');
+console.log(title === null || title === void 0 ? void 0 : title.textContent);
+var message = document.querySelector('#message');
+console.log(message.placeholder);
+console.log("Hola de nuevo");
