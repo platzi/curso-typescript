f17d946126a8417f02113e008c93aa7068dff669
aminespinoza10 <amin.espinoza@outlook.com>
2025-02-27 12:01:12 -0600

    Ejercicio de pruebas agregado

---
+++ Jest/Usuario.test.ts
@@ -0,0 +1,26 @@
+import { Usuario } from './usuario';
+
+describe('Usuario Class', () => {
+    beforeEach(() => {
+        usuario = new Usuario('Amin', 'amin@example.com', 'Developer', 10, true);
+      });
+
+      test('should return the correct name', () => {
+        expect(usuario.getName()).toBe('Amin');
+      });
+    
+      test('should return the correct profile', () => {
+        const expectedProfile = 'El usuario es Amin, se dedica a Developer y tiene 10 años de experiencia.';
+        expect(usuario.getProfile()).toBe(expectedProfile);
+      });
+    
+      test('should return the correct active status', () => {
+        expect(usuario.isStillActive()).toBe(true);
+      });
+    
+      test('should return false for inactive user', () => {
+        usuario = new Usuario('Amin', 'amin@example.com', 'Developer', 10, false);
+        expect(usuario.isStillActive()).toBe(false);
+      });
+    
+});
